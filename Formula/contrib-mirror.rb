8988fa57413c7879c75d36a023ab6b8e284a792d
yuvrajangadsingh <yuvrajangad.s@gmail.com>
2026-02-07 02:32:23 +0530

    Add contrib-mirror formula v1.0.0

---
+++ Formula/contrib-mirror.rb
@@ -0,0 +1,27 @@
+class ContribMirror < Formula
+  desc "Mirror private work contributions to your GitHub profile"
+  homepage "https://github.com/yuvrajangadsingh/private-work-contributions-mirror"
+  url "https://github.com/yuvrajangadsingh/private-work-contributions-mirror/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "95acb88b56fe20e39eb14f76457d70fd7265b5a52c605255eed32ad20c567026"
+  license "MIT"
+
+  depends_on "git"
+
+  def install
+    bin.install "sync.sh" => "contrib-mirror"
+    libexec.install "setup.sh"
+    # Patch setup path in sync.sh to point to libexec
+    inreplace bin/"contrib-mirror", "$SCRIPT_DIR/setup.sh", "#{libexec}/setup.sh"
+  end
+
+  def caveats
+    <<~EOS
+      Run the setup wizard to configure:
+        contrib-mirror --setup
+    EOS
+  end
+
+  test do
+    assert_match "contrib-mirror", shell_output("#{bin}/contrib-mirror --version")
+  end
+end
